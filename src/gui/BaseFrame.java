b7f60fdd83edbac3abdfa34cb80f0b3e0ac98cce
Dmitriy <kolesnik982011@gmail.com>
2025-08-01 18:53:53 +0300

    add BaseFrame

---
+++ src/gui/BaseFrame.java
@@ -0,0 +1,28 @@
+package gui;
+
+import javax.swing.JFrame;
+
+
+/*
+ * Creating an abstract class helps us setup the blueprint that our GUIs will follow, for example
+ * in each of the GUIs they will have the same size and will need to invoke their own addGuiComponents()
+ * which will be unique to each subclass
+ */
+public abstract class BaseFrame extends JFrame {
+    public BaseFrame(String title) {
+        init(title);
+    }
+
+    private void init(String title) {
+        setTitle(title);
+        setSize(800, 600);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(null);
+        setResizable(false);
+        setLocationRelativeTo(null);
+
+        addGuiComponents();
+    }
+
+    protected abstract void addGuiComponents();
+}
